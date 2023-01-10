6de2e5b3fb9b69200086466e00d9e7c2744b0e89
KevinAlexV <32602411+KevinAlexV@users.noreply.github.com>
2023-01-10 07:32:53 -0800

    cleanup

--- sheetLoader.js
+++
@@ -1,8 +0,0 @@
-function loadSheet() {
-	var loadString = window.prompt("Enter Character Sheet JSON String");
-	characterSheet = JSON.parse(loadString);
-}
-
-function saveSheet(sheet) {
-	alert("Copy this JSON String for later use:\n\n" + JSON.stringify(sheet));
-}
