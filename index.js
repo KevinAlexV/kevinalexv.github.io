3e7b04bf8fbd41385626c73dd37e64a4d0a2b416
KevinAlexV <32602411+KevinAlexV@users.noreply.github.com>
2023-01-12 10:06:06 -0800

    Update to logic

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 var currentContainer = 2;
 var numofContainers = 4;
+
+var farLeftCon = document.getElementById("farLeft");
+var farRightCon = document.getElementById("farRight");
+var leftCon = document.getElementById("left");
+var rightCon = document.getElementById("right");
+var centerCon = document.getElementById("center");
+var onHoldCon = document.getElementById("onHold");
+
 //Grab first container by ID "child1"
 //var childWidth = document.getElementById("child1").offsetWidth;
 function checkEdge(event) 
@@ -53,6 +61,7 @@ function moveRight(event)
 
 function moveLeft(event)
 {
+    //Gather values of the container we need to swap to.
     currentLeftContainer = currentContainer - 1; 
     currentRightContainer = currentContainer + 1;
 
@@ -80,13 +89,28 @@ function moveLeft(event)
         nextLeftContainer = numofContainers;
     }
 
+    //Grab the IDs for each container
     var rightElement = document.getElementById("child"+(rightContainer));
     var leftElement = document.getElementById("child"+(leftContainer));
     var centerElement = document.getElementById("child"+(currentContainer));
-    
-    document.getElementById("child"+(rightContainer)).style.opacity=0;
-    document.getElementById("child"+(currentContainer)).style.opacity=.5;
-    document.getElementById("child"+(leftContainer)).style.opacity=1;
+    var nextRightElement = document.getElementById("child"+(nextRightContainer));
+    var nextLeftElement = document.getElementById("child"+(nextLeftContainer));
 
     currentContainer = leftContainer;
+
+    let left = document.getElementById('left');
+    let right = document.getElementById('right');
+    let redBox = document.querySelector('.element');
+    left.onclick = function(){
+      let pos = parseInt(window.getComputedStyle(redBox,null).getPropertyValue("left"), 10);
+      let move = pos - 40;
+      redBox.style.left = move + 'px'
+    }
+    right.onclick = function(){
+      let pos = parseInt(window.getComputedStyle(redBox,null).getPropertyValue("left"), 10);
+      let move = pos + 40;
+      redBox.style.left = move + 'px'
+    }
+
+
 }
